fcf3d9e6df4a6371fcae466a26908374a590f747
rohitsingh2901 <singhrohitrrr@gmail.com>
2023-08-19 14:22:23 +0530

    added audio in video capturing

--- src/components/allSaves/YourVideo.js
+++ src/components/allSaves/YourVideo.js
@@ -19,18 +19,24 @@ const YourVideo= () => {
 
   const handleStartCaptureClick = React.useCallback(() => {
     setCapturing(true);
-    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-      mimeType: "video/webm"
-    });
-    mediaRecorderRef.current.addEventListener(
-      "dataavailable",
-      handleDataAvailable
-    );
-    mediaRecorderRef.current.start();
-  }, [webcamRef, setCapturing, mediaRecorderRef,handleDataAvailable]);
+    const constraints = {
+      audio: true,
+      video: true,
+    };
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        webcamRef.current.srcObject = stream;
+        mediaRecorderRef.current = new MediaRecorder(stream, {
+          mimeType: "video/webm"
+        });
+        mediaRecorderRef.current.addEventListener("dataavailable", handleDataAvailable);
+        mediaRecorderRef.current.start();
+      });
+  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
 
   const handleStopCaptureClick = React.useCallback(() => {
     mediaRecorderRef.current.stop();
+    webcamRef.current.srcObject.getTracks().forEach(track => track.stop());
     setCapturing(false);
   }, [mediaRecorderRef, setCapturing]);
 
@@ -54,21 +60,20 @@ const YourVideo= () => {
   return (
     <>
       <div className='flex justify-center'>
-        <button onClick={()=>setShowCam(true)} className='mx-4 text-black font-medium  bg-green-600 btn-sm'>Show Camera</button>
-        <button onClick={()=>setShowCam(false)} className='mx-4 text-white font-medium  bg-red-600 btn-sm'>Stop Camera</button>
+        <button onClick={()=>setShowCam(true)} className='mx-4 text-black font-medium  bg-green-600 btn-sm'>Open Camera</button>
+        <button onClick={()=>setShowCam(false)} className='mx-4 text-white font-medium  bg-red-600 btn-sm'>Close Camera</button>
       </div>
-      <div className='flex justify-center my-3'>
+      <div className=' my-3'>
       {capturing ? (
         <button className='mx-4 text-white font-medium  bg-red-600 btn-lg' onClick={handleStopCaptureClick}>Stop Capture</button>
         ) : (
-          <button disabled={showCam===true ? false : true} className={`mx-4 text-black font-medium  bg-green-600 btn-lg ${showCam === false ? "cursor-not-allowed	" : ""
-        }`} onClick={handleStartCaptureClick}>Start Capture</button>
+          <button disabled={showCam===true ? false : true} className={`mx-4 text-black font-medium  bg-green-600 btn-lg ${showCam === false ? "cursor-not-allowed	" : ""}`} onClick={handleStartCaptureClick}>Start Capture</button>
         )}
       {recordedChunks.length > 0 && (
         <button onClick={handleDownload}>Download</button>
         )}
       </div>
-      {showCam ? (<div className='flex justify-center'><Webcam className='w-25'  audio={false} ref={webcamRef} /></div>):('')}
+      {showCam ? (<div><Webcam className='w-25'  audio={false} ref={webcamRef} /></div>):('')}
       {recordedChunks.length > 0 && (
         <div className='flex justify-center'>
           <video controls src={URL.createObjectURL(recordedChunks[0])}></video>
@@ -78,4 +83,4 @@ const YourVideo= () => {
   );
 };
 
-export default YourVideo
+export default YourVideo;
