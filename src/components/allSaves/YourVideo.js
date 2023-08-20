7e4deeb73beaf38dbf98474722e844d943709d01
rohitsingh2901 <singhrohitrrr@gmail.com>
2023-08-20 11:37:17 +0530

    API for saving audio

--- src/components/allSaves/YourVideo.js
+++ src/components/allSaves/YourVideo.js
@@ -7,6 +7,7 @@ const YourVideo= () => {
   const [capturing, setCapturing] = React.useState(false);
   const [recordedChunks, setRecordedChunks] = React.useState([]);
   const [showCam, setShowCam] = React.useState(false);
+  const [data, setData] = React.useState({})
 
   const handleDataAvailable = React.useCallback(
     ({ data }) => {
@@ -40,43 +41,101 @@ const YourVideo= () => {
     setCapturing(false);
   }, [mediaRecorderRef, setCapturing]);
 
-  const handleDownload = React.useCallback(() => {
-    if (recordedChunks.length) {
-      const blob = new Blob(recordedChunks, {
-        type: "video/webm"
-      });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style = "display: none";
-      a.href = url;
-      a.download = "react-webcam-stream-capture.webm";
-      a.click();
-      window.URL.revokeObjectURL(url);
-      setRecordedChunks([]);
+  // const handleDownload = React.useCallback(() => {
+  //   if (recordedChunks.length) {
+  //     const blob = new Blob(recordedChunks, {
+  //       type: "video/webm"
+  //     });
+  //     const url = URL.createObjectURL(blob);
+  //     const a = document.createElement("a");
+  //     document.body.appendChild(a);
+  //     a.style = "display: none";
+  //     a.href = url;
+  //     a.download = "react-webcam-stream-capture.webm";
+  //     a.click();
+  //     window.URL.revokeObjectURL(url);
+  //     setRecordedChunks([]);
+  //   }
+  // }, [recordedChunks]);
+
+  const handleSave = async () => {
+    if (recordedChunks.length === 0) {
+      return;
     }
-  }, [recordedChunks]);
+  
+    try {
+      const formData = new FormData();
+      const videoBlob = new Blob(recordedChunks, { type: 'video/webm' });
+      formData.append('videoFile', videoBlob, 'video.webm'); 
+      formData.append('name', data.Name);
+      formData.append('email', data.Email);
+      formData.append('id', data._id);
+  
+      for (const key of formData.keys()) {
+        console.log(key, formData.get(key));
+      }
+  
+      const response = await fetch('http://localhost:5000/upload-video', {
+        method: 'POST',
+        body: formData,
+      });
+  
+      if (response.ok) {
+        console.log('Video saved successfully!');
+        const res = await response.json();
+        console.log(res);
+      } else {
+        console.error('Failed to save video.');
+      }
+    } catch (error) {
+      console.error('Error saving video:', error);
+    }
+  };
+  
+
+  React.useEffect(() => {
+    const run2 = async () => {
+      const response = await fetch("http://localhost:5000/getuser", {
+        method: "POST",
+        headers: { 'auth-token': `${localStorage.getItem('token')}` }
+      });
+
+      if (response.ok) {
+        setData(await response.json());
+      } else {
+        console.error("Please login first");
+      }
+    }
+    run2()
+  }, [])
+  
 
   return (
     <>
-      <div className='flex justify-center'>
+      <div>
         <button onClick={()=>setShowCam(true)} className='mx-4 text-black font-medium  bg-green-600 btn-sm'>Open Camera</button>
         <button onClick={()=>setShowCam(false)} className='mx-4 text-white font-medium  bg-red-600 btn-sm'>Close Camera</button>
       </div>
-      <div className=' my-3'>
+      <div className='mx-10 my-3'>
       {capturing ? (
         <button className='mx-4 text-white font-medium  bg-red-600 btn-lg' onClick={handleStopCaptureClick}>Stop Capture</button>
         ) : (
-          <button disabled={showCam===true ? false : true} className={`mx-4 text-black font-medium  bg-green-600 btn-lg ${showCam === false ? "cursor-not-allowed	" : ""}`} onClick={handleStartCaptureClick}>Start Capture</button>
-        )}
-      {recordedChunks.length > 0 && (
-        <button onClick={handleDownload}>Download</button>
+          <>
+          <button title={showCam===true ? '' : 'Open camera first'} disabled={showCam===true ? false : true} className={`mx-4 text-black font-medium  bg-green-600 btn-lg ${showCam === false ? "cursor-not-allowed	" : ""}`} onClick={handleStartCaptureClick}>Start Capture</button>
+          <span></span>
+          </>
         )}
       </div>
-      {showCam ? (<div><Webcam className='w-25'  audio={false} ref={webcamRef} /></div>):('')}
+      {showCam ? (<div><Webcam className='border-solid border-8 border-red-500 mx-3 w-25'  audio={false} ref={webcamRef} /></div>):('')}
       {recordedChunks.length > 0 && (
-        <div className='flex justify-center'>
-          <video controls src={URL.createObjectURL(recordedChunks[0])}></video>
+        <div className='my-3 mx-3'>
+          {/* {recordedChunks.length > 0 && (
+        <button className='text-black font-medium  bg-green-600 btn-sm' onClick={handleDownload}>Download</button>
+        )} */}
+          {recordedChunks.length > 0 && (
+        <button className='text-black font-medium  bg-green-600 btn-sm' onClick={handleSave}>Save</button>
+        )}
+          <video className='w-25' controls src={URL.createObjectURL(recordedChunks[0])}></video>
         </div>
       )}
     </>
